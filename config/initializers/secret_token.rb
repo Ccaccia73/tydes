9aaf2a46203a8f890341fba2d4c26d382f6b45a3
Claudio Caccia <c.caccia@libero.it>
2012-11-14 11:47:28 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tydes::Application.config.secret_token = 'c66268d9d9288e2ea9e1843787b711763a575a83906db773cf255db5a444263a20f05c706899e3343df8f1d8c82524b0df2b5772ff60a155ddc0961c9f6ebedd'
