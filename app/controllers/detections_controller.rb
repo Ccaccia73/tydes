3a0ad758802f4beff1fc27b3831c456ac79a18e8
Claudio Caccia <c.caccia@libero.it>
2012-12-10 14:13:31 +0100

    added all images

--- app/controllers/detections_controller.rb
+++ app/controllers/detections_controller.rb
@@ -1,9 +1,29 @@
 class DetectionsController < ApplicationController
-  def new
-  	@detection = Detection.new
-  end
+	def new
+		@detection = Detection.new
+	end
 
-  def show
-  	@detection = Detection.find(params[:id])
-  end
+	def show
+		@detection = Detection.find(params[:id])
+	end
+
+	def create
+		@detection = Detection.new(params[:detection])
+
+		begin
+			tmp = (0..4).map{65.+(rand(26)).chr}.join
+			[1,3].each do |k|
+				tmp[k] = (1+rand(9)).to_s
+			end
+		end while (Detection.find_by_code(tmp))
+
+		@detection.code = tmp
+
+    	if @detection.save
+    		flash[:success] = "Welcome to the training phase!"
+      		redirect_to @detection
+    	else
+			render 'new'
+		end
+	end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
 	factory :detection do
 		user	1
 		value	"y"*30
-		code	"aabb"
+		code	"A1A1A"
 	end
 end
