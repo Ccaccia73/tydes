9f87ef65286a57b243edab8ad6e244a1d0582e56
Claudio Caccia <c.caccia@libero.it>
2012-12-12 08:52:38 +0100

    modified varie.js

--- app/assets/javascripts/varie.js
+++ app/assets/javascripts/varie.js
@@ -1,4 +1,22 @@
 
-$(document).ready(function(){
-	alert(“The DOM is Ready!”);
-});
+(function($){
+	$('#shortly').countdown({until: shortly,
+							 onExpiry: liftOff,
+							 onTick: watchCountdown});
+
+	$('#shortlyStart').click(function() {
+			alert('Start!');
+			shortly = new Date();
+			shortly.setSeconds(shortly.getSeconds() + 5.5);
+			$('#shortly').countdown('option', {until: shortly});
+	});
+
+	function liftOff() {
+		alert('We have lift off!');
+		//window.location.replace("http://www.google.com");
+	}
+
+	function watchCountdown(periods) {
+			$('#monitor').text('Just ' + periods[5] + ' minutes and ' + periods[6] + ' seconds to go'); 
+	}
+})(jQuery);
